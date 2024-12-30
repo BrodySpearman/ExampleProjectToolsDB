67b22952b24adb40bdbb44ff2a0536cae44e90b7
Brody Spearman <brodyspearman@gmail.com>
2024-12-30 17:42:29 -0600

    Added new html tables. Added function for fetching variable table info.

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, inspect, MetaData, Table, Column, Integer, String, SmallInteger, text, select
 from sqlalchemy.engine import reflection
+from sqlalchemy.sql import func
+from jinja2 import Template
 import mysql.connector
 
-from sqlalchemy.sql import func
 
+### DATABASE CONNECTION ###
+
+# Delete these variables and encrypt them elsewhere soon.
 dbhost = "localhost"
 dbuser = "root"
 dbpass = "roothost1"
@@ -15,7 +19,6 @@ dbname = "stateline_tools_db"
 
 app = Flask(__name__)
 
-# SqlAlchemy Initialization
 engine = create_engine(
         url="mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}".format(
             dbuser, dbpass, dbhost, port, dbname
@@ -23,6 +26,9 @@ engine = create_engine(
 
 conn = engine.connect()
 metadata_obj = MetaData(schema='stateline_tools_db')
+
+
+### TABLE INITIALIZATION ###
 
 avail_databases = ['tool', 'employee', 'checkout']
 
@@ -44,18 +50,24 @@ checkout_table = Table(
     autoload_with=engine
 )
 
-temp_query = "SELECT * FROM tool"
-tool_result = conn.execute(text(temp_query))
-columns = []
 
-for elem in tool_result.cursor.description: 
-    columns.append(elem[0]) 
+### TABLE FUNCTIONS ###
 
-dataset = tool_result.fetchall()
+def get_table_info(tblnme):
+    get_table_info.columns = []
 
-print(dataset)
+    temp_query = f"SELECT * FROM {tblnme}"
+    tool_result = conn.execute(text(temp_query))
 
-print(columns)
+    for elem in tool_result.cursor.description: 
+        get_table_info.columns.append(elem[0]) 
+
+    get_table_info.dataset = tool_result.fetchall()
+    print(get_table_info.dataset)
+    print(get_table_info.columns)
+    
+
+### APP ROUTES ###
 
 # Flask command line operation (Windows):
 # set FLASK_APP=app
@@ -64,5 +76,12 @@ print(columns)
 
 @app.route('/')
 def index():
-    return render_template('bodycontent.html', dataset=dataset, columns=columns)
-    
+    get_table_info('tool')
+    return render_template('bodycontent.html', get_table_info=get_table_info)
+
+
+@app.route('/employees')
+def employee():
+    get_table_info('employee')
+    return render_template('bodycontent.html', get_table_info=get_table_info)
+
