60e018920891e56d0fe2c48b7373443b388b7295
Brody Spearman <brodyspearman@gmail.com>
2025-01-23 15:58:02 -0600

    Added Node.js and node libraries for editor function.
    Basic Editor panel added. Npm configured with node.js to run editor dependencies. Numerous formatting changes made to tables and scripts. Deprecating the "Add Records" button in favor for admin menu.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, inspect, MetaData, Table, Column, Integer, String, SmallInteger, text, select
+
+from sqlalchemy import create_engine, inspect, MetaData, Table, Column, text, select
 from sqlalchemy.engine import reflection
 from sqlalchemy.sql import func
 from jinja2 import Template
-import mysql.connector
+from flaskext.mysql import MySQL
+import pymysql
+import json
 
+# Flask command line operation (Windows):
+# set FLASK_APP=app
+# set FLASK_ENV=development
+# flask run
 
 ### DATABASE CONNECTION ###
 # Delete these variables, change and encrypt them elsewhere soon.
@@ -18,16 +25,20 @@ port = "3306"
 dbname = "stateline_tools_db"
 
 app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = dbuser
+app.config['MYSQL_DATABASE_PASSWORD'] = dbpass
+app.config['MYSQL_DATABASE_DB'] = dbname
+app.config['MYSQL_DATABASE_HOST'] = dbhost
+mysql.init_app(app)
 
 engine = create_engine(
         url="mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}".format(
             dbuser, dbpass, dbhost, port, dbname
         ), echo=True)
 
-conn = engine.connect()
 metadata_obj = MetaData(schema='stateline_tools_db')
-
-
 
 ### TABLE INITIALIZATION ###
 
@@ -55,6 +66,7 @@ checkout_table = Table(
 ### TABLE FUNCTIONS ###
 
 def get_table_info(tblnme):
+    conn = engine.connect()
     get_table_info.columns = []
 
     temp_query = f"SELECT * FROM {tblnme}"
@@ -66,11 +78,8 @@ def get_table_info(tblnme):
     get_table_info.dataset = tool_result.fetchall()
     print(get_table_info.dataset)
     print(get_table_info.columns)
-    
-# Flask command line operation (Windows):
-# set FLASK_APP=app
-# set FLASK_ENV=development
-# flask run
+    conn.close()
+
 
 ### APP ROUTES ###
 
@@ -83,13 +92,52 @@ def tool_table_show():
     get_table_info('tool')
     return render_template('tool_table.html', get_table_info=get_table_info)
 
+@app.route('/ajaxtools', methods = ['GET', 'POST'])
+def ajaxtools():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        if request.method == 'POST':
+            draw = request.form['draw']
+            print(draw)
+
+            cursor.execute("SELECT * FROM tool")
+            tool_list = cursor.fetchall()
+
+            data = []
+            for row in tool_list:
+                data.append({
+                    'ToolID': row['ToolID'],
+                    'Type': row['Type'],
+                    'ToolName': row['ToolName'],
+                    'Brand': row['Brand'],
+                    'SKU': row['SKU'],
+                    'SerialNum': row['SerialNum'],
+                    'Description': row['Description'],
+                })
+            
+            response = {
+                'draw': draw,
+                'iTotalRecords': 20,
+                'iTotalDisplayRecords': 20,
+                'aaData': data,
+            }
+            return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
 @app.route('/checkouts', methods = ['GET', 'POST'])
 def checkout_table_show():
+
     get_table_info('checkout')
     return render_template('checkout_table.html', get_table_info=get_table_info)
 
 @app.route('/employees', methods = ['GET', 'POST'])
 def employee_table_show():
+
     get_table_info('employee')
     return render_template('employee_table.html', get_table_info=get_table_info)
 
---
+++ static/scripts/editor.js
@@ -0,0 +1,23 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+let e = require('datatables.net-editor-server');
+ 
+let Editor = e.Editor;
+let Field = e.Field;
+let Validate = e.Validate;
+let Format = e.Format;
+
+let knex = require('knex');
+ 
+module.exports = knex({
+    client: 'mysql',
+ 
+    connection: {
+        database:    'stateline_tools_db',
+        host:        'localhost',
+        password:    'roothost1',
+        user:        'root',
+        dateStrings: true
+    }
+});
